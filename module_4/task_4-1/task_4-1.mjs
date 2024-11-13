31c67b46c538ecc1e1745fa69e4c74adc322634e
Oda Opsanger <odasop@uia.no>
2024-11-13 22:22:03 +0100

    Task 4-1 done.

--- module_4/task_4-1/task_4-1.mjs
+++ module_4/task_4-1/task_4-1.mjs
@@ -3,36 +3,157 @@ import { initPrintOut, printOut, newLine } from "../../common/script/utils.mjs";
 initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+const AccountType = {
+  Normal: "Brukskonto",
+  Saving: "Sparekonto",
+  Credit: "Kredittkonto",
+  Pension: "Pensjonskonto"
+};
+
+printOut(`${AccountType.Normal}, ${AccountType.Saving}, ${AccountType.Credit}, ${AccountType.Pension}`);
 printOut(newLine);
 
+
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+class TAccount {
+  #type;
+  #balance;
+  #withdrawCount;
+  #currencyType;
+
+  constructor(aType) {
+    this.#type = aType;
+    this.#balance = 0;
+    this.#withdrawCount = 0;
+    this.#currencyType = "NOK"; 
+  }
+
+  toString() {
+    return this.#type;
+  }
+
+  setType(aType) {
+    const oldType = this.#type;
+    this.#type = aType;
+    this.#withdrawCount = 0;
+    printOut(`Account is changed from ${oldType} to ${this.#type}.`);
+  }
+
+  getBalance() {
+    return this.#balance.toFixed(2); 
+  }
+
+  deposit(aAmount, aCurrency = "NOK") {
+    if (aCurrency !== this.#currencyType) {
+      aAmount = this.#convertCurrency(aAmount, aCurrency, this.#currencyType);
+    }
+    this.#balance += aAmount;
+    this.#withdrawCount = 0; 
+    printOut(`Deposit of ${aAmount.toFixed(2)}${CurrencyTypes[this.#currencyType].denomination}, new balance is ${this.getBalance()}${CurrencyTypes[this.#currencyType].denomination}.`);
+  }
+
+  withdraw(aAmount, aCurrency = "NOK") {
+    if (aCurrency !== this.#currencyType) {
+      aAmount = this.#convertCurrency(aAmount, aCurrency, this.#currencyType);
+    }
+
+    switch (this.#type) {
+      case AccountType.Pension:
+        printOut("You can't withdraw from a Pensjonskonto!");
+        return;
+      case AccountType.Saving:
+        if (this.#withdrawCount >= 3) {
+          printOut("You can't withdraw from a Sparekonto more than three times!");
+          return;
+        }
+        this.#withdrawCount++;
+        break;
+      default:
+        break;
+    }
+
+    if (aAmount <= this.#balance) {
+      this.#balance -= aAmount;
+      printOut(`Withdrawal of ${aAmount.toFixed(2)}${CurrencyTypes[this.#currencyType].denomination}, new balance is ${this.getBalance()}${CurrencyTypes[this.#currencyType].denomination}.`);
+    } else {
+      printOut(`Withdrawal of ${aAmount.toFixed(2)} failed. Insufficient balance.`);
+    }
+  }
+
+  setCurrencyType(aType) {
+    if (this.#currencyType === aType) return; 
+
+    const oldCurrency = CurrencyTypes[this.#currencyType].name;
+    this.#balance = this.#convertCurrency(this.#balance, this.#currencyType, aType);
+    this.#currencyType = aType;
+    printOut(`The account currency has changed from ${oldCurrency} to ${CurrencyTypes[this.#currencyType].name}.`);
+    printOut(`New Balance is ${this.getBalance()}${CurrencyTypes[this.#currencyType].denomination}.`);
+  }
+
+  #convertCurrency(amount, fromCurrency, toCurrency) {
+    const fromValue = CurrencyTypes[fromCurrency].value;
+    const toValue = CurrencyTypes[toCurrency].value;
+    return (amount * toValue) / fromValue;
+  }
+}
+
+const CurrencyTypes = {
+  NOK: { value: 1.0, name: "Norske kroner", denomination: "kr" },
+  EUR: { value: 0.0985, name: "Europeiske euro", denomination: "€" },
+  USD: { value: 0.1091, name: "United States dollar", denomination: "$" },
+  GBP: { value: 0.0847, name: "Pound sterling", denomination: "£" },
+  INR: { value: 7.8309, name: "Indiske rupee", denomination: "₹" },
+  AUD: { value: 0.1581, name: "Australske dollar", denomination: "A$" },
+  PHP: { value: 6.5189, name: "Fillipinske peso", denomination: "₱" },
+  SEK: { value: 1.0580, name: "Svenske kroner", denomination: "kr" },
+  CAD: { value: 0.1435, name: "Canadiske dollar", denomination: "C$" },
+  THB: { value: 3.3289, name: "Thai baht", denomination: "฿" }
+};
+
+const myAccount = new TAccount(AccountType.Normal);
+printOut(`myAccount = ${myAccount.toString()}`);
+myAccount.setType(AccountType.Saving);
+printOut(`myAccount = ${myAccount.toString()}`);
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+myAccount.deposit(100);
+myAccount.withdraw(25);
+printOut(`My account balance is ${myAccount.getBalance()}kr.`);
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+myAccount.setType(AccountType.Saving);
+myAccount.deposit(25);
+myAccount.withdraw(30);
+myAccount.withdraw(30);
+myAccount.withdraw(30);
+myAccount.withdraw(30);
+myAccount.setType(AccountType.Pension);
+myAccount.withdraw(10); 
+myAccount.setType(AccountType.Saving);
+myAccount.withdraw(10); 
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+myAccount.setCurrencyType("NOK");
+myAccount.deposit(150);
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+myAccount.setCurrencyType("SEK"); 
+myAccount.setCurrencyType("USD"); 
+myAccount.setCurrencyType("NOK");
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+myAccount.deposit(12, "USD");
+myAccount.withdraw(10, "GBP");
+myAccount.setCurrencyType("CAD");
+myAccount.setCurrencyType("INR");
+myAccount.withdraw(150, "SEK"); 
 printOut(newLine);
+
+/*
+printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
+Virker som oppgave 8 har kommet med fra forrige oppgavetekst. Løste den i innlevering 3.3.*/
